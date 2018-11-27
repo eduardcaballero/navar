63a08b70eeaa0d722ab75f4ea6ace39764ae8e9a
caballero <eduardcaballerop@gmail.com>
2018-11-27 17:43:33 -0500

    cambios2

--- navarpsi/settings.py
+++ navarpsi/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'navarpsi.wsgi.application'
 #         'HOST': '127.0.0.1',
 #         'PORT': '3306',
 #     }
-}
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
